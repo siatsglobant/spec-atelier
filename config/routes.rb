386dd9e08ee455695557f4d5d167f07441619b9d
Sophia-Sergio <san.storres@gmail.com>
2019-10-08 23:16:18 -0300

    Add api to scope routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,11 @@
 Rails.application.routes.draw do
-  resources :sessions, only: %i[create]
-  delete :logout, to: 'sessions#logout'
-  get :logged_in, to: 'sessions#logged_in'
-  get :anything, to: 'sessions#anything'
 
-  resources :registrations, only: %i[create]
+  namespace :api do
+    resources :sessions, only: %i[create]
+    delete :logout, to: 'sessions#logout'
+    get :logged_in, to: 'sessions#logged_in'
+    get :anything, to: 'sessions#anything'
+    resources :registrations, only: %i[create]
+  end
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
