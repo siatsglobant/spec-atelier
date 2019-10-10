28e8059cd9da15307600ac69b9eac0516267dc74
Sophia-Sergio <san.storres@gmail.com>
2019-10-09 22:55:33 -0300

    Add Api to routes

---
+++ app/controllers/api/registrations_controller.rb
@@ -0,0 +1,14 @@
+module Api
+  class RegistrationsController < ApplicationController
+    def create
+      user = User.create!(email:                 params['user']['email'],
+                          password:              params['user']['password'],
+                          password_confirmation: params['user']['password'])
+      if user
+        render json: { status: :created, logged_in: true, user: user }
+      else
+        render json: { status: 500 }
+      end
+    end
+  end
+end
