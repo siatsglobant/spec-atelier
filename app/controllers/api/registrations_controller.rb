fa20b164e06346ac2c0c3701921e56ae25394da1
Sophia-Sergio <san.storres@gmail.com>
2019-10-16 20:46:55 -0300

    Add proper behaviour to failed user creation

--- app/controllers/api/registrations_controller.rb
+++ app/controllers/api/registrations_controller.rb
@@ -1,10 +1,10 @@
 module Api
   class RegistrationsController < ApplicationController
     def create
-      user = User.create!(email:                 params['user']['email'],
-                          password:              params['user']['password'],
-                          password_confirmation: params['user']['password'])
-      if user
+      user = User.new(email:                 params['user']['email'],
+                      password:              params['user']['password'],
+                      password_confirmation: params['user']['password'])
+      if user.save
         start_session(user)
         render json: { logged_in: true, user: user, jwt: current_session.token }, status: :created
       else
