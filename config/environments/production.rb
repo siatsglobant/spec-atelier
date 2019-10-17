fa68282ccdd9909fc1a0ac730f4abd33896fba79
Sophia-Sergio <san.storres@gmail.com>
2019-10-17 18:20:54 -0300

    Perform sending emails

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -56,6 +56,7 @@ Rails.application.configure do
   config.action_mailer.perform_caching = false
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.default_url_options = { host: ENV['BACK_DOMAIN'] }
+  config.action_mailer.perform_deliveries = true
 
   # Ignore bad email addresses and do not raise email delivery errors.
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
