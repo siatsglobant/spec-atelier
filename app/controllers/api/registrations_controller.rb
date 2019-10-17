7e48380e65f8c719b1c08c28b07f1e615e1bc07c
Sophia-Sergio <san.storres@gmail.com>
2019-10-17 13:52:32 -0300

    change status to :confict when users email already exists

--- app/controllers/api/registrations_controller.rb
+++ app/controllers/api/registrations_controller.rb
@@ -8,7 +8,7 @@ module Api
         start_session(user)
         render json: { logged_in: true, user: user, jwt: current_session.token }, status: :created
       else
-        render json: { error: user.errors.to_json }, status: :internal_server_error
+        render json: { error: user.errors.to_json }, status: :conflict
       end
     end
   end
