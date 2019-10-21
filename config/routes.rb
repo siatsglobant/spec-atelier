5417d296c9ce0e2d95b6babae16f8433d967aa81
Sophia-Sergio <san.storres@gmail.com>
2019-10-21 10:14:59 -0300

    Add google auth routes

--- config/routes.rb
+++ config/routes.rb
@@ -4,11 +4,12 @@ Rails.application.routes.draw do
     resources :sessions, only: %i[create]
     put :logout, to: 'sessions#logout'
     get :logged_in, to: 'sessions#logged_in'
-    get :anything, to: 'sessions#email_testing'
     resources :registrations, only: %i[create]
 
     get :password_forgot, to: 'passwords#forgot'
     get :password_reset, to: 'passwords#reset'
   end
+  get 'auth/:provider/callback', to: 'api/sessions#google_auth'
+  get 'auth/failure', to: 'api/sessions#google_auth_failure'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
