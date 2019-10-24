ab0fe4174c24ae37d2d835413d4547ee480935db
Sophia-Sergio <san.storres@gmail.com>
2019-10-24 11:15:49 -0300

    Add google auth feature

--- app/controllers/api/sessions_controller.rb
+++ app/controllers/api/sessions_controller.rb
@@ -1,15 +1,17 @@
 module Api
   class SessionsController < ApplicationController
-    before_action :set_current_user, except: %i[create logout]
-    before_action :valid_session, except: %i[create]
+    before_action :set_current_user, except: %i[create logout google_auth google_auth_failure]
+    before_action :valid_session, except: %i[create google_auth google_auth_failure]
 
     def create
-      user = User.find_by(email: params['user']['email']).try(:authenticate, params['user']['password'])
-      if user.present?
+      user = User.find_by(email: params['user']['email'])
+      if user.try(:authenticate, params['user']['password']).present?
         start_session(user)
-        render json: { logged_in: true, user: user, jwt: current_session.token }, status: :created
+        render json: { logged_in: true, user: BasicUserPresenter.to_print(current_user) }, status: :created
+      elsif user&.google_token.present?
+        render json: { error: 'you signed up with google' }, status: :not_found
       else
-        render json: { error: 'Email or password not found' }, status: :not_found
+        render json: { error: 'email or password not found' }, status: :not_found
       end
     end
 
@@ -24,6 +26,17 @@ module Api
     def logout
       end_session
       render json: { logged_out: true }, status: :ok
+    end
+
+    def google_auth
+      access_token = request.env["omniauth.auth"]
+      user = omniouth_handler_login(access_token)
+      start_session(user)
+      render json: { logged_in: true, user: BasicUserPresenter.to_print(current_user) }, status: :created
+    end
+
+    def google_auth_failure
+      render json: { error: 'google auth failure' }, status: :internal_server_error
     end
   end
 end
