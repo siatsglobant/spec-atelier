84cd421a8d17c195aefe560655d943942d855229
Sophia-Sergio <san.storres@gmail.com>
2019-10-24 11:25:22 -0300

    Add expires date to session

--- db/schema.rb
+++ db/schema.rb
@@ -21,6 +21,7 @@ ActiveRecord::Schema.define(version: 2019_10_17_180721) do
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.boolean "active", default: true
+    t.datetime "expires"
     t.index ["user_id"], name: "index_sessions_on_user_id"
   end
 
