55815b93279035eab84f2b6690208854ef456178
Sophia-Sergio <san.storres@gmail.com>
2019-10-24 11:21:08 -0300

    Refactor, a little one

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,7 +4,7 @@ class User < ApplicationRecord
   has_one :session
 
   def generate_password_token!
-    update(reset_password_token: generate_token, reset_password_sent_at: Time.now.utc)
+    update(reset_password_token: SecureRandom.hex(10), reset_password_sent_at: Time.zone.now)
   end
 
   def password_token_valid?
@@ -13,11 +13,5 @@ class User < ApplicationRecord
 
   def reset_password!(password)
     update(reset_password_token: nil, password: password)
-  end
-
-  private
-
-  def generate_token
-    SecureRandom.hex(10)
   end
 end
