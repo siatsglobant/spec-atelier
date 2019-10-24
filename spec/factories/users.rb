5d7bfe5e30414959736b6c9a1f7ee1bf49e2e1ff
Sophia-Sergio <san.storres@gmail.com>
2019-10-24 11:47:05 -0300

    Add trait user as one with password token

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -2,5 +2,10 @@ FactoryBot.define do
   factory :user do
     email    { 'email@test.com' }
     password { '123456' }
+
+    trait :with_password_token do
+      reset_password_token { 'token' }
+      reset_password_sent_at { Time.zone.now }
+    end
   end
 end
