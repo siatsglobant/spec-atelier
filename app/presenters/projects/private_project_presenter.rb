beabcfcf30ab27beee997f9940580df8656a4427
Sophia-Sergio <san.storres@gmail.com>
2019-11-13 13:36:46 -0300

    Add id to list of json keys response

--- app/presenters/projects/private_project_presenter.rb
+++ app/presenters/projects/private_project_presenter.rb
@@ -1,6 +1,6 @@
 module Projects
   class PrivateProjectPresenter < Presenter
-    will_print :name, :project_type, :work_type, :country, :city, :delivery_date, :status
+    will_print :id, :name, :project_type, :work_type, :country, :city, :delivery_date, :status
 
     def name
       project.name
