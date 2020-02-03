6f6431b95d8232720488256e427eae5b05e0f20a
Sophia-Sergio <san.storres@gmail.com>
2020-02-03 01:36:46 -0300

    Add fix for heroku deploy fail

--- config/initializers/global_variables.rb
+++ config/initializers/global_variables.rb
@@ -1,2 +1,4 @@
-lookup_tables = JSON.parse(LookupTable.all.to_json)
-Object.const_set('LOOKUP_TABLE', lookup_tables)
+if ActiveRecord::Base.connection.table_exists? 'lookup_tables'
+  lookup_tables = JSON.parse(LookupTable.all.to_json)
+  Object.const_set('LOOKUP_TABLE', lookup_tables)
+end
