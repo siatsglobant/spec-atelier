d3d4e5729ae494f46b190f7f06490f8e8ce5ff47
Sophia-Sergio <san.storres@gmail.com>
2020-03-06 23:17:20 -0300

    Add sections and items_by_section

--- app/controllers/api/general_controller.rb
+++ app/controllers/api/general_controller.rb
@@ -5,5 +5,16 @@ module Api
     def cities
       render json: { cities: CITIES.values.flatten }, status: :ok
     end
+
+    def sections
+      sections_sidebar = Section.all.order(:show_order).map {|section| { eng_name: section.eng_name, name: section.name }}
+      render json: { sections: sections_sidebar}
+    end
+
+    def items_by_section
+      section = Section.find_by(eng_name: params[:section])
+      items = section.items.as_json(only: %i[id name])
+      render json: { section: section.name,  items: items }, status: :ok
+    end
   end
 end
