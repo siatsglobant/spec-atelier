02b1248541c56528362d9b6aa6b5a0ac0e490d59
Sophia-Sergio <san.storres@gmail.com>
2020-03-29 18:24:18 -0300

    Add subitem and item relation

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,4 +1,8 @@
 class Product < ApplicationRecord
   include MetaLookupTable
   belongs_to :brand
+  belongs_to :subitem, optional: true
+  has_one :item, through: :subitem
+  # has_many :images, as: :owner, class_name: 'Image', autosave: true
+
 end
