4907467c949dba49c602c8dd1b4a08d3313a15ce
Sophia-Sergio <san.storres@gmail.com>
2020-03-29 18:21:47 -0300

    Move sections actin to sections controller

--- app/controllers/api/general_controller.rb
+++ app/controllers/api/general_controller.rb
@@ -6,11 +6,6 @@ module Api
       render json: { cities: CITIES.values.flatten }, status: :ok
     end
 
-    def sections
-      sections_sidebar = Section.all.order(:show_order).map {|section| { eng_name: section.eng_name, name: section.name }}
-      render json: { sections: sections_sidebar}
-    end
-
     def items_by_section
       section = Section.find_by(eng_name: params[:section])
       items = section.items.as_json(only: %i[id name])
