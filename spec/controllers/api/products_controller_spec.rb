70bf51c6c4b67194a3f9d565571d4303f870ec1e
Sophia-Sergio <san.storres@gmail.com>
2020-04-07 22:18:42 -0400

    Add spec

--- spec/controllers/api/products_controller_spec.rb
+++ spec/controllers/api/products_controller_spec.rb
@@ -2,8 +2,8 @@ RSpec.describe Api::ProductsController, type: :controller do
   let(:user)           { create(:user) }
   let(:no_logged_user) { create(:user) }
   let(:session)        { create(:session, user: user, token: session_token(user)) }
-  let(:products)       { create_list(:product, 10)}
-
+  let(:products)       { create_list(:product, 10) }
+  let(:product)        { create(:product) }
 
   describe '#show' do
     context 'without session' do
@@ -12,12 +12,20 @@ RSpec.describe Api::ProductsController, type: :controller do
     end
 
     context 'with valid session' do
-      before { request.headers['Authorization'] = "Bearer #{session.token}"}
+      before do
+        request.headers['Authorization'] = "Bearer #{session.token}"
+      end
 
       it 'returns a resource' do
-        product = create(:product)
+        image1 = create(:image, owner: product, order: 0)
+        image2 = create(:image, owner: product, order: 1)
+
         get :show, params: { id: product.id }
+
         expect(json['product']['name']).to eq(product.name)
+        expect(json['product']['images'].first['order']).to eq(image1.order)
+        expect(json['product']['images'].first['urls']).to eq(image1.all_formats.as_json)
+        expect(json['product']['images'].second['urls']).to eq(image2.all_formats.as_json)
       end
 
       it 'returns a another resource' do
@@ -26,5 +34,4 @@ RSpec.describe Api::ProductsController, type: :controller do
       end
     end
   end
-
 end
